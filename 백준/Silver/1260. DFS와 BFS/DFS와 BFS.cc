5378ed598ec3074206844069fcb3cf4241bfe484
유태희 <123617670+youtaehew@users.noreply.github.com>
2025-04-02 17:26:04 +0900

    [Silver II] Title: DFS와 BFS, Time: 4 ms, Memory: 2180 KB -BaekjoonHub

---
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
@@ -0,0 +1,64 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<algorithm>
+
+using namespace std;
+
+vector<int> arr[1001];
+bool visited[1001];
+
+void DFS(int v) {
+	cout << v << " ";
+	visited[v] = true;
+	for (int i = 0; i < arr[v].size(); i++) {
+		int next = arr[v][i];
+		if (!visited[next]) {
+			DFS(next);
+		}
+	}
+}
+
+void BFS(int v) {
+	queue<int> q;
+	q.push(v);
+	visited[v] = true;
+
+	while (!q.empty()) {
+		int a = q.front();
+		q.pop();
+		cout << a << " ";
+
+		for (int i = 0; i < arr[a].size(); i++) {
+			int b = arr[a][i];
+			if (!visited[b]) {
+				q.push(b);
+				visited[b] = true;
+			}
+		}
+	}
+}
+
+int main() {
+	int n, m, v;
+	cin >> n >> m >> v;
+
+	for (int i = 0; i < m; i++) {
+		int x, y;
+		cin >> x >> y;
+
+		arr[x].push_back(y);
+		arr[y].push_back(x);
+	}
+
+	for (int i = 1; i <= n; i++) {
+		sort(arr[i].begin(), arr[i].end());
+	}
+
+	fill(visited, visited + 1001, false);
+	DFS(v);
+	cout << "\n";
+
+	fill(visited, visited + 1001, false);
+	BFS(v);
+}
